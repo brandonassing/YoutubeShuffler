f728ab4b3b53636c767108340edff3dbfb4cd536
brandon.assing <brandon.assing@ibm.com>
2017-12-08 10:49:42 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <iframe title="First Aid" width="560" height="315" src="https://www.youtube.com/embed/Vmb1tqYqyII" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+    </div>
+    );
+  }
+}
+
+export default App;
